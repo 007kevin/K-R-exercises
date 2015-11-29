ac1c392afc6ff50e7b648fd5b93df5950a37aec6
Kevin-Chroot <kevinkim1991@gmail.com>
2015-11-29 06:34:50 -0500

    cleaned up unneeded files

--- test2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int c = EOF;
-    int x = c;
-    printf("%u\n", x);
-
-    return 0;
-}
--- test7.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    char *s = "hello %d\n";
-    printf(s, 10);    
-    return 0;
-}
-
-
